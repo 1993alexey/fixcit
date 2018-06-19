70f6f04e4814d6c2cbd669bdd62313a88a146930
Alex <1993alexey@gmail.com>
2018-06-19 17:59:27 -0600

    Footer modifications

--- footer.php
+++ footer.php
@@ -1,11 +1,10 @@
-<!-- Footer -->
-<footer class="page-footer font-small blue pt-4 mt-4">
-
-    <!-- Copyright -->
-    <div class="footer-copyright text-center ">© 2018 Copyright:
-        <a href="https://mdbootstrap.com/bootstrap-tutorial/" class="copyright-link"> Fixcit.com</a>
-    </div>
-    <!-- Copyright -->
-
-</footer>
-<!-- Footer -->
+<!--<!-- Footer -->
+<!--<footer class="page-footer font-small blue pt-4 mt-4">-->
+<!---->
+<!--    <!-- Copyright -->
+<!--    <div class="footer-copyright text-center ">© 2018 Copyright:-->
+<!--        <a href="https://mdbootstrap.com/bootstrap-tutorial/" class="copyright-link"> Fixcit.com</a>-->
+<!--    </div>-->
+<!--    <!-- Copyright -->
+<!---->
+<!--</footer>-->
